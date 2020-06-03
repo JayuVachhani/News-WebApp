eea77409ac514938deb8cd530d81eb07ee614a85
jay <jay.160410116139@gmail.com>
2020-06-03 16:44:16 +0530

    added database folder

---
+++ database files/flask_users.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: flask
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'wda','pilomil637@provlst.com','$5$rounds=535000$PY.bVwLRjaB9SjhQ$HRR96hSteYAiCot3R1Kqh70GiS2pVYKt6VhTnasaby6'),(5,'wdad','pilomil637@provlst.com','$5$rounds=535000$aF331bm3DWHU8iJB$qizUasBhydoILCNRe1b32SJwZ0KDVXjH53B4Me6uA.B'),(6,'ddsds','ankur@gmail.com','$5$rounds=535000$w4Z.O/jbJTuipdRy$Q4CIKBJHvT27HG3fvA54r/6GfgRY.PBQhbnafk7MQr6');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-02 18:22:55
--- newsapp.py
+++ newsapp.py
@@ -1,160 +1,267 @@
-import sqlite3
+# re for regular expression
+import re
 from flask import Flask, render_template, request, redirect, url_for, flash,session
 from datetime import datetime
+from flaskext.mysql import MySQL
 from passlib.hash import sha256_crypt
-from wtforms import Form, TextField, validators, StringField, PasswordField, BooleanField, form
-
 
 # create instance of the Flask class
 # here parameter of Flask constructor is give current module's name
 app = Flask(__name__)
 
+mysql = MySQL()
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1232587'
+app.config['MYSQL_DATABASE_DB'] = 'flask'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 # display current time
 today = datetime.now()
 print (today.strftime("%Y - %m - %d  %H : %M : %S"))
 
 #here decorator is used to navigate the URL
-# whenever user go to the home page route function fire the home function and return the data
+# whenever user go to the home page route function fire the register mandatory function and return the data
 @app.route("/")
 def reg_mand():
     # passing reference of date variable
     return render_template("register.html",today=today)
 
-class RegistrationForm(Form):
-    username  = StringField("Username",[validators.length(min=4,max=20),validators.DataRequired()])
-    email =  StringField("Email",[validators.length(min=6,max=30),validators.DataRequired()])
-    password = PasswordField("Password",[validators.DataRequired()])
-
-
 # register page
 @app.route("/register/",methods=['GET','POST'])
 def register():
-    # passing reference of date variable
-    form = RegistrationForm(request.form)
+    msg=""
+    # check if request is post or not
+    if request.method == 'POST' and 'username' in request.form and 'email' in request.form and 'password' in request.form:
+        # store the user-data in variables coming from form
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        # create hashed password
+        hashed = sha256_crypt.encrypt(str(password))
 
-    if request.method == "POST" and form.validate():
-        try:
-            username = form.username.data
-            email  = form.email.data
-            password = form.password.data
+        # create connection with the database
+        conn = mysql.connect()
+        # create cursor for connection
+        cursor = conn.cursor()
 
-            with sqlite3.connect("news.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT into users(username,email,password) VALUES (?, ?, ?)", (username, email, password))
+        # Check if account exists in database or not
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
 
-                con.commit()
-                print(cur.fetchall())
-                cur.close()
+        # fetch that particular user
+        account = cursor.fetchone()
 
-                return redirect(url_for('login'))
-        except:
-            con.rollback()
+        # If account exists show error and validation checks
+        if account:
+            msg='Account already exists!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
 
+        # regular expression
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z]+', username):
+            msg = 'Username must contain only characters!'
 
-
-    return render_template("register.html", today=today,form=form)
-
-
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO users VALUES (null,%s, %s, %s)', (username, email,hashed))
+            # commit all changes
+            conn.commit()
+            # after close all actions
+            conn.close()
+            cursor.close()
+            flash('You have successfully registered!')
+            return redirect(url_for('login'))
+    return render_template('register.html', msg=msg)
 # Login page
 @app.route("/login/",methods=['GET','POST'])
 def login():
-    msg = 'msg'
-    if request.method == 'POST':
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        userpass = request.form['password']
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        result = cursor.execute("SELECT * FROM users WHERE username = %s",[username])
+        # Fetch one record and return result
 
-            username = request.form['username']
-            password = request.form['password']
-            with sqlite3.connect("news.db") as con:
-                cur = con.cursor()
-                sqlselct = "SELECT * FROM users WHERE username = ? and password = ?"
-                print('username : {0} :::: password : {1}'.format(username, password))
-                cur.execute(sqlselct, (username, password,))
-                data = cur.fetchone()
-                # print('12312312313123123123123123')
-                # print(data)
-                # print(data[1])
-            # data = cur.fetchall()
-                if data:
-                    print("Welcome" + data[1])
-                    id_num = data[0]
-                    session['logged_in'] = True
-                    session['admin'] = False
-                    session['username'] = username
-                    session['id'] = id_num
-                    flash('Logged In Successfully!!!')
-                    return redirect(url_for('home'))
-            # else:
-            #     flash('Invalid Login', 'danger')
-            #     return redirect(url_for('login'))
-                else:
-                    flash('User not found', 'danger')
-                    return redirect(url_for('login'))
+        if result > 0:
+            data = cursor.fetchone()
+            # retrieve password from database
+            password = data[3]
+            # retrieve id
+            id_num = data[0]
 
-    return render_template('login.html',today=today)
+            # match both hased and user password
+            if sha256_crypt.verify(userpass, password):
 
+                # if true session will be started
+                session['loggedin'] = True
+                session['admin'] = False
+
+                # store user in session
+                session['username'] = username
+                session['id'] = id_num
+                msg='Logged In Successfully!!!', 'success'
+                return redirect(url_for('home',msg=msg))
+            else:
+                msg = 'Invalid Login'
+                return render_template('login.html',msg=msg)
+        else:
+            msg='User not found'
+            return render_template('login.html',msg=msg)
+
+    return render_template('login.html',msg=msg)
 
 
 @app.route("/home/")
 def home():
-    return render_template("index.html",today=today)
+    # first check if user is logged in or not
+    if 'loggedin' in session:
+        # if true it render the request to particular destination
+        return render_template("index.html",username=session['username'],today=today)
+    # if not logged in redirect to login page
+    return redirect(url_for('login'))
 
 
 # news page
 @app.route("/news/",methods = ['GET','POST'])
 def news():
-
-    if request.method == 'POST':
+    msg=''
+    if 'loggedin' in session:
+        if request.method == 'POST' and 'headlines' in request.form and 'description' in request.form and 'author' in request.form and 'category' in request.form:
             headlines  = request.form['headlines']
             description = request.form['description']
             author = request.form['author']
             category = request.form['category']
-            with sqlite3.connect("news.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT into news(headlines,description,author,category) VALUES (?, ?, ?, ?)", (headlines,description,author,category))
-                print('headlines : {0} :::: description : {1} :::: author : {2} :::: category : {3}'.format(headlines,description,author,category))
-                con.commit()
-                print(cur.fetchall())
-                cur.close()
-                return redirect(url_for('shownews'))
-    return render_template("news.html",today=today,form=form)
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("INSERT into news(headlines,description,author,category) VALUES (%s, %s, %s, %s)", (headlines,description,author,category))
+            # print('headlines : {0} :::: description : {1} :::: author : {2} :::: category : {3}'.format(headlines,description,author,category))
+            conn.commit()
+            flash("News Added Successfully")
+            # print(cursor.fetchall())
+            cursor.close()
+            return redirect(url_for('shownews'))
+        else:
+            msg="Something went wrong please try again"
+            return render_template("news.html",today=today,msg=msg)
+    return redirect(url_for('login'))
 
 # show news
-@app.route("/shownews/")
+@app.route("/shownews/",methods = ['GET','POST'])
 def shownews():
-    con = sqlite3.connect("news.db")
-    con.row_factory = sqlite3.Row
-
-    cur = con.cursor()
-    cur.execute("select * from news")
-
-    rows = cur.fetchall()
-    return render_template("shownews.html", rows=rows,today=today)
+    if 'loggedin' in session:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM news')
+        rows = cursor.fetchall()
+        # print(rows)
+        return render_template("shownews.html", rows=rows, today=today)
+    return redirect(url_for('login'))
 
 
 # contact page
 @app.route("/contact/",methods = ['GET','POST'])
 def contact():
-    if request.method == 'POST':
+    msg=""
+    if 'loggedin' in session:
+        if request.method == 'POST' and 'fullname' in request.form and 'email' in request.form and 'heading' in request.form and 'subject' in request.form:
             name  = request.form['fullname']
             email = request.form['email']
             heading = request.form['heading']
             subject = request.form['subject']
-            with sqlite3.connect("news.db") as con:
-                cur = con.cursor()
-                cur.execute("INSERT into contact(name,email,heading,subject) VALUES (?, ?, ?, ?)", (name,email,heading,subject))
-                print('name : {0} :::: email : {1} :::: heading : {2} :::: subject : {3}'.format(name,email,heading,subject))
-                con.commit()
-                flash("Contact successfully added")
-                print(cur.fetchall())
-                cur.close()
+            conn = mysql.connect()
+            cursor = conn.cursor()
 
-    return render_template("contact.html",today=today)
+            cursor.execute("INSERT into contact(name,email,heading,subject) VALUES (%s, %s, %s, %s)", (name,email,heading,subject))
+            print('name : {0} :::: email : {1} :::: heading : {2} :::: subject : {3}'.format(name,email,heading,subject))
+            conn.commit()
+
+            print(cursor.fetchall())
+            msg = "Your Query Successfully Registered"
+            cursor.close()
+
+        return render_template("contact.html",today=today,msg=msg)
+    return redirect(url_for('login'))
+
+# get id of particular request which is auto incremented
+@app.route("/editnews/<id>",methods = ['GET','POST'])
+
+# edit
+def edit(id):
+    try:
+        print("id is: "+id)
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * from news where id = %s",(id,))
+        rows=cursor.fetchone()
+        print(rows)
+        cursor.close()
+        return render_template('updatenews.html',rows=rows,today=today)
+    except Exception as e:
+        print(e)
+
+# update
+@app.route("/updatenews/<id>",methods = ['GET','POST'])
+def update(id):
+    if 'loggedin' in session:
+        try:
+            print("update id"+id)
+            
+            headlines = request.form['headlines']
+            print("headline",headlines)
+            description = request.form['description']
+            author = request.form['author']
+            category = request.form['category']
+            print('headlines : {0} :::: description : {1} :::: author : {2} :::: category : {3}'.format(headlines,description,author,category))
+            print("updatesasdsa id"+id)
+            if headlines and description and author and category:
+
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.execute('Update news set headlines = %s, description = %s, author = %s, category = %s WHERE id = %s',(headlines,description,author,category,id))
+                flash("News Updated Successfully")
+                conn.commit()
+                cursor.fetchall()
+                cursor.close()
+                conn.close()
+                return redirect(url_for('shownews'))
+            else:
+                msg="Somethong went wrong"
+
+                return render_template('updatenews.html',msg=msg)
+        except Exception as e:
+            print(e)
+    return redirect(url_for('login'))
+
+# delete
+@app.route("/delete/<id>")
+def delete(id):
+    try:
+        print('id:{0}'.format(id))
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        result=cursor.execute("DELETE FROM news WHERE id=%s", (id,))
+        print("Res : ",result)
+        cursor.fetchone()
+        flash('News deleted successfully!')
+        conn.commit()
+        conn.close()
+        return redirect(url_for('shownews'))
+
+    except Exception as e:
+        print(e)
 
 
 # logout
 @app.route('/logout/')
 def logout():
-	session.pop('username', None)
-	return redirect('/')
+    session.pop('username', None)
+    return redirect('/')
 
 
 
